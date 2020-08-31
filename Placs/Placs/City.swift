915be6773c643f456b7e3130e9efca2b029a627e
jesseleeruiz <jesseleeruiz@gmail.com>
2020-08-31 13:12:44 -1000

    Added City file. Added SearchVC and embedded TV into it. Connected DataSource and Delegate.

---
+++ Placs/Placs/City.swift
@@ -0,0 +1,16 @@
+//
+//  City.swift
+//  Placs
+//
+//  Created by Jesse Ruiz on 8/31/20.
+//  Copyright © 2020 Jesse Ruiz. All rights reserved.
+//
+
+import Foundation
+import CoreLocation
+
+struct City {
+    var name: String
+    var country: String
+    var coordinates: CLLocationCoordinate2D
+}
---
+++ Placs/Placs/SearchViewController.swift
@@ -0,0 +1,34 @@
+//
+//  SearchViewController.swift
+//  Placs
+//
+//  Created by Jesse Ruiz on 8/31/20.
+//  Copyright © 2020 Jesse Ruiz. All rights reserved.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+    
+    // MARK: - Outlets
+    @IBOutlet var tableView: UITableView!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
