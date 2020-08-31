1930f16eb7e9965b115aea1b9ea56818cf4fd6d7
jesseleeruiz <jesseleeruiz@gmail.com>
2020-08-31 12:52:23 -1000

    Added Xcode project.

---
+++ Placs/Placs/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Placs
+//
+//  Created by Jesse Ruiz on 8/31/20.
+//  Copyright © 2020 Jesse Ruiz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
