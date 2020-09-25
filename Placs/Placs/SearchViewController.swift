00072e6afa1fa535116fdf1f9284a7a6259f030d
jesseleeruiz <jesseleeruiz@gmail.com>
2020-09-24 16:51:01 -1000

    Added image assets.

--- Placs/Placs/SearchViewController.swift
+++ Placs/Placs/SearchViewController.swift
@@ -19,7 +19,6 @@ class SearchViewController: UIViewController {
     
     var allCities = [City]()
     var matchingCities = [City]()
-    
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -38,21 +37,8 @@ class SearchViewController: UIViewController {
             
             allCities.append(newCity)
         }
-        
         allCities.sort()
     }
-
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
 
 extension SearchViewController: UITableViewDelegate, UITableViewDataSource {
